8654727bc828d73cf3b6f6ce1213a0df273cc7fd
ChrisZyy3 <48314211+ChrisZyy3@users.noreply.github.com>
2025-12-19 22:39:14 +0800

    Increase header z-index

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -55,7 +55,7 @@ export const Header = () => {
   });
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 border-b border-white/10">
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-30 shadow-md shadow-secondary px-0 sm:px-2 border-b border-white/10">
       <div className="navbar-start w-auto lg:w-1/2">
         <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="relative w-10 h-10">
